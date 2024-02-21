bde5cfb4287b3cda5ef71bcb3a8d2f00cb07b5ba
Admin <admin@Admins-MacBook-Air.local>
2024-02-21 11:13:13 +0530

    commiting main.java file in master

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class main
+{
+    public static void  main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int h=sc.nextInt();
+        int add=addition(a,b);
+        int div=divsion(add);
+        int mul=multiplication(div,h);
+        System.out.println("Area is: "+mul);
+    }
+}
